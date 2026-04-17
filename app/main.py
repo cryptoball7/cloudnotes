d1f47fc84c42911039c690852af0c4db8ef4778d
cryptoball7 <cryptoball7@gmail.com>
2026-04-17 15:36:33 -0700

    Day 2: PostgreSQ: + models setup

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from app.core.database import engine, Base
+
+from app.models import user, note  # IMPORTANT
 
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def read_root():
-    return {"message": "CloudNotes API is running"}
-
+    return {"message": "CloudNotes API running with DB"}
