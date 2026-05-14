283bbb9389dbd723ac3b580c98d92c091f6f21f6
cryptoball7 <cryptoball7@gmail.com>
2026-05-13 22:49:33 -0700

    Added app/core/config.py

--- app/core/security.py
+++ app/core/security.py
@@ -1,9 +1,7 @@
 from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
-
-SECRET_KEY = "supersecretkey"
-ALGORITHM = "HS256"
+from app.core.config import SECRET_KEY, ALGORITHM
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
